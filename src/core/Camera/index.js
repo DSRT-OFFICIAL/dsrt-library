6a096c145c85fb26f58f8f41a591a2c532d17d93
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 06:01:43 +0700

    Create index.js

---
+++ src/core/Camera/index.js
@@ -0,0 +1,6 @@
+import Camera from './Camera.js';
+import PerspectiveCamera from './PerspectiveCamera.js';
+import OrthographicCamera from './OrthographicCamera.js';
+
+export { Camera, PerspectiveCamera, OrthographicCamera };
+export default Camera;
