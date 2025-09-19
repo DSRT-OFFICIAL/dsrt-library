e76ff344f05d681522ee4ccdc809204271ff2114
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 06:04:53 +0700

    Create index.js

---
+++ src/core/Light/index.js
@@ -0,0 +1,8 @@
+import Light from './Light.js';
+import AmbientLight from './AmbientLight.js';
+import DirectionalLight from './DirectionalLight.js';
+import PointLight from './PointLight.js';
+import SpotLight from './SpotLight.js';
+
+export { Light, AmbientLight, DirectionalLight, PointLight, SpotLight };
+export default Light;
