979df5e23eff0df7b29329f5e80eb605c184178c
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 06:00:20 +0700

    Create index.js

---
+++ src/core/Scene/index.js
@@ -0,0 +1,7 @@
+import Scene from './Scene.js';
+import FogScene from './FogScene.js';
+import SkyboxScene from './SkyboxScene.js';
+import ShadowScene from './ShadowScene.js';
+
+export { Scene, FogScene, SkyboxScene, ShadowScene };
+export default Scene;
