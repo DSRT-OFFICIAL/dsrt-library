e5c7266f05f8ad328180caa9dc815b2c3fd348f7
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 01:30:31 +0700

    Create Grey.js

---
+++ src/math/Color/Grey.js
@@ -0,0 +1,13 @@
+import Color from '../Color.js';
+const Grey = {
+  black:      new Color(0.00, 0.00, 0.00),
+  dimgray:    new Color(0.41, 0.41, 0.41),
+  gray:       new Color(0.50, 0.50, 0.50),
+  darkgray:   new Color(0.66, 0.66, 0.66),
+  silver:     new Color(0.75, 0.75, 0.75),
+  lightgray:  new Color(0.83, 0.83, 0.83),
+  gainsboro:  new Color(0.86, 0.86, 0.86),
+  whitesmoke: new Color(0.96, 0.96, 0.96),
+  white:      new Color(1.00, 1.00, 1.00),
+};
+export default Grey;
