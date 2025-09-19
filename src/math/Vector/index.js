24e37b732e63621e070d305707e9c2a92f27e3cb
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 02:51:49 +0700

    Create index.js

---
+++ src/math/Vector/index.js
@@ -0,0 +1,6 @@
+import Vector2 from './Vector2.js';
+import Vector3 from './Vector3.js';
+import Vector4 from './Vector4.js';
+
+export { Vector2, Vector3, Vector4 };
+export default { Vector2, Vector3, Vector4 };
