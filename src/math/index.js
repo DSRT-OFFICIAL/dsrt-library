ce92bfe84f5569cec62160e3da0d751bc1a4104e
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 00:59:20 +0700

    Create index.js

---
+++ src/math/index.js
@@ -0,0 +1,6 @@
+export { default as Vector2 } from './Vector2.js';
+export { default as Vector3 } from './Vector3.js';
+export { default as Vector4 } from './Vector4.js';
+export { default as Matrix4 } from './Matrix4.js';
+export { default as Quaternion } from './Quaternion.js';
+export { default as Color } from './Color.js';
