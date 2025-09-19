58524c4c0eb3d2dcf9f07e64684d263ceacd9972
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 03:02:56 +0700

    Update index.js

--- src/math/index.js
+++ src/math/index.js
@@ -1,6 +1,10 @@
-export { default as Vector2 } from './Vector2.js';
-export { default as Vector3 } from './Vector3.js';
-export { default as Vector4 } from './Vector4.js';
-export { default as Matrix4 } from './Matrix4.js';
-export { default as Quaternion } from './Quaternion.js';
-export { default as Color } from './Color.js';
+// Import specific modules
+import { Vector3, Matrix4, Quaternion, Red } from './math';
+
+// Pakai default object
+import MathLib from './math';
+
+const v = new MathLib.Vector3(1, 2, 3);
+const m = new MathLib.Matrix4().identity();
+const q = MathLib.Quaternion.identity();
+const color = MathLib.Red;  // '#ff0000'
