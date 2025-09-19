b35d32475277d44162cde9104a8d5dae058df727
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 06:14:54 +0700

    Create index.js

---
+++ src/geometry/index.js
@@ -0,0 +1,7 @@
+import Geometry from './Geometry.js';
+import BoxGeometry from './BoxGeometry.js';
+import SphereGeometry from './SphereGeometry.js';
+import PlaneGeometry from './PlaneGeometry.js';
+
+export { Geometry, BoxGeometry, SphereGeometry, PlaneGeometry };
+export default Geometry;
