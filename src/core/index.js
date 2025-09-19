84603fff75830d1333386b5a667d2a6a3e5950d2
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 05:57:51 +0700

    Update index.js

--- src/core/index.js
+++ src/core/index.js
@@ -1,1 +1,7 @@
-import { Object3D, Scene, Camera, Light } from './core';
+import Object3D from './Object3D/Object3D.js';
+import Scene from './Scene/index.js';
+import Camera from './Camera/index.js';
+import Light from './Light/index.js';
+
+export { Object3D, Scene, Camera, Light };
+export default { Object3D, Scene, Camera, Light };
