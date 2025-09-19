ea609821c6b27651d1d81ab963dc6547dd7be1ba
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 01:41:31 +0700

    Create Matrix3.js

---
+++ src/math/Matrix/Matrix3.js
@@ -0,0 +1,35 @@
+export default class Matrix3 {
+  constructor() {
+    this.elements = new Float32Array(9);
+    this.identity();
+  }
+
+  identity() {
+    const e = this.elements;
+    e[0] = 1; e[3] = 0; e[6] = 0;
+    e[1] = 0; e[4] = 1; e[7] = 0;
+    e[2] = 0; e[5] = 0; e[8] = 1;
+    return this;
+  }
+
+  clone() {
+    const m = new Matrix3();
+    m.elements.set(this.elements);
+    return m;
+  }
+
+  multiply(matrix) {
+    const a = this.elements;
+    const b = matrix.elements;
+    const te = new Float32Array(9);
+
+    for (let row=0; row<3; row++) {
+      for (let col=0; col<3; col++) {
+        te[row+col*3] = a[row]*b[col*3] + a[row+3]*b[col*3+1] + a[row+6]*b[col*3+2];
+      }
+    }
+
+    this.elements = te;
+    return this;
+  }
+}
