f4334099bddb00d2b0a0d5449e27d62f96889d3d
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 01:31:26 +0700

    Create Red.js

---
+++ src/math/Color/Red.js
@@ -0,0 +1,14 @@
+import Color from '../Color.js';
+const Red = {
+  red:        new Color(1.00, 0.00, 0.00),
+  darkred:    new Color(0.55, 0.00, 0.00),
+  firebrick:  new Color(0.70, 0.13, 0.13),
+  crimson:    new Color(0.86, 0.08, 0.24),
+  indianred:  new Color(0.80, 0.36, 0.36),
+  lightcoral: new Color(0.94, 0.50, 0.50),
+  salmon:     new Color(0.98, 0.50, 0.45),
+  darksalmon: new Color(0.91, 0.59, 0.48),
+  lightsalmon:new Color(1.00, 0.63, 0.48),
+  tomato:     new Color(1.00, 0.39, 0.28),
+};
+export default Red;
