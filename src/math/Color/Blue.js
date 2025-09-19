c02e0341c275736130b1d77aaba2b94d00c8d859
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 01:31:07 +0700

    Create Blue.js

---
+++ src/math/Color/Blue.js
@@ -0,0 +1,15 @@
+import Color from '../Color.js';
+const Blue = {
+  blue:        new Color(0.00, 0.00, 1.00),
+  darkblue:    new Color(0.00, 0.00, 0.55),
+  mediumblue:  new Color(0.00, 0.00, 0.80),
+  navy:        new Color(0.00, 0.00, 0.50),
+  royalblue:   new Color(0.25, 0.41, 0.88),
+  dodgerblue:  new Color(0.12, 0.56, 1.00),
+  deepskyblue: new Color(0.00, 0.75, 1.00),
+  lightskyblue:new Color(0.53, 0.81, 0.98),
+  skyblue:     new Color(0.53, 0.81, 0.92),
+  lightblue:   new Color(0.68, 0.85, 0.90),
+  steelblue:   new Color(0.27, 0.51, 0.71),
+};
+export default Blue;
