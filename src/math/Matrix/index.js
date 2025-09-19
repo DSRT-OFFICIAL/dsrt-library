3e737ac99bdb2c69ef46b40b4472ed43b6158899
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 01:42:56 +0700

    Create index.js

---
+++ src/math/Matrix/index.js
@@ -0,0 +1,6 @@
+import Matrix2 from './Matrix2.js';
+import Matrix3 from './Matrix3.js';
+import Matrix4 from './Matrix4.js';
+
+export { Matrix2, Matrix3, Matrix4 };
+export default { Matrix2, Matrix3, Matrix4 };
