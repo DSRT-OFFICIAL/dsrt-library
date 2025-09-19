43c4ba52345446aeb8e7df4e28ce526a882d2110
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 01:29:50 +0700

    Create Orange.js

---
+++ src/math/Color/Orange.js
@@ -0,0 +1,14 @@
+import Color from '../Color.js';
+const Orange = {
+  orange:       new Color(1.00, 0.65, 0.00),
+  darkorange:   new Color(1.00, 0.55, 0.00),
+  coral:        new Color(1.00, 0.50, 0.31),
+  tomato:       new Color(1.00, 0.39, 0.28),
+  orangered:    new Color(1.00, 0.27, 0.00),
+  sandybrown:   new Color(0.96, 0.64, 0.38),
+  peru:         new Color(0.80, 0.52, 0.25),
+  chocolate:    new Color(0.82, 0.41, 0.12),
+  sienna:       new Color(0.63, 0.32, 0.18),
+  bisque:       new Color(1.00, 0.89, 0.77),
+};
+export default Orange;
