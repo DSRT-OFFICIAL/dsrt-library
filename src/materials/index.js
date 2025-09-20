dddf7f0b1a1d5ab161b7ceba5ae6dd76709f7a29
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 15:04:05 +0700

    Create index.js

---
+++ src/materials/index.js
@@ -0,0 +1,36 @@
+import Material from './Material.js';
+import BasicMaterial from './BasicMaterial.js';
+import LambertMaterial from './LambertMaterial.js';
+import PhongMaterial from './PhongMaterial.js';
+import StandardMaterial from './StandardMaterial.js';
+import DepthMaterial from './DepthMaterial.js';
+import NormalMaterial from './NormalMaterial.js';
+import ShaderMaterial from './ShaderMaterial.js';
+
+import BasicShaderMaterial from './BasicShaderMaterial.js';
+import LambertShaderMaterial from './LambertShaderMaterial.js';
+import PhongShaderMaterial from './PhongShaderMaterial.js';
+import StandardShaderMaterial from './StandardShaderMaterial.js';
+import DepthShaderMaterial from './DepthShaderMaterial.js';
+import NormalShaderMaterial from './NormalShaderMaterial.js';
+import SkyboxShaderMaterial from './SkyboxShaderMaterial.js';
+
+export {
+  Material,
+  BasicMaterial,
+  LambertMaterial,
+  PhongMaterial,
+  StandardMaterial,
+  DepthMaterial,
+  NormalMaterial,
+  ShaderMaterial,
+  BasicShaderMaterial,
+  LambertShaderMaterial,
+  PhongShaderMaterial,
+  StandardShaderMaterial,
+  DepthShaderMaterial,
+  NormalShaderMaterial,
+  SkyboxShaderMaterial,
+};
+
+export default Material;
