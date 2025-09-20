f97e07853dd7a837ed4af2ec00915f18138467b4
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 15:38:51 +0700

    Update index.js

--- src/math/index.js
+++ src/math/index.js
@@ -1,19 +1,5 @@
-// VECTOR
-export { Vector2, Vector3, Vector4 } from './Vector/index.js';
-
-// MATRIX
-export { Matrix2, Matrix3, Matrix4 } from './Matrix/index.js';
-
-// QUATERNION
+// Export semua modul dari subfolder
+export * from './Color/index.js';
+export * from './Matrix/index.js';
+export * from './Vector/index.js';
 export { default as Quaternion } from './Quaternion.js';
-
-// COLOR (jika ada)
-export { default as Color } from './Color/index.js';
-
-// DEFAULT EXPORT
-export default {
-  Vector2, Vector3, Vector4,
-  Matrix2, Matrix3, Matrix4,
-  Quaternion,
-  Color
-};
