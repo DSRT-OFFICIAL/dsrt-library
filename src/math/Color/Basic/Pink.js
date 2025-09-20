be04b001574bffe4c60cc88ce349567e1cfe5290
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:11:29 +0700

    Create Pink.js

---
+++ src/math/Color/Basic/Pink.js
@@ -0,0 +1,16 @@
+import Color from '../Color.js';
+
+const Pink = {
+  pink:        new Color(1.0, 0.75, 0.80),
+  lightpink:   new Color(1.0, 0.71, 0.76),
+  hotpink:     new Color(1.0, 0.41, 0.71),
+  deeppink:    new Color(1.0, 0.08, 0.58),
+  palevioletred:new Color(0.86, 0.44, 0.58),
+  mediumvioletred:new Color(0.78, 0.08, 0.52),
+  lavenderblush:new Color(1.0, 0.94, 0.96),
+  mistyrose:   new Color(1.0, 0.89, 0.88),
+  pinksherbet: new Color(0.98, 0.68, 0.82),
+  fuchsia:     new Color(1.0, 0.0, 1.0),
+};
+
+export default Pink;
