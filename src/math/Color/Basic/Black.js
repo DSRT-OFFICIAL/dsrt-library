91c50cf1af2c811f7102bc24e33f41d8df754114
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:10:47 +0700

    Create Black.js

---
+++ src/math/Color/Basic/Black.js
@@ -0,0 +1,16 @@
+import Color from '../Color.js';
+
+const Black = {
+  black:       new Color(0.0, 0.0, 0.0),
+  dimgray:     new Color(0.41, 0.41, 0.41),
+  gray:        new Color(0.5, 0.5, 0.5),
+  darkslategray:new Color(0.18, 0.31, 0.31),
+  slategray:   new Color(0.44, 0.5, 0.56),
+  lightblack:  new Color(0.27, 0.27, 0.27),
+  charcoal:    new Color(0.21, 0.27, 0.31),
+  jet:         new Color(0.15, 0.15, 0.15),
+  raven:       new Color(0.09, 0.09, 0.09),
+  ebony:       new Color(0.18, 0.18, 0.18),
+};
+
+export default Black;
