34648ed56b4ed43800130501e27f0e6e459190ec
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:12:51 +0700

    Create Green.js

---
+++ src/math/Color/Basic/Green.js
@@ -0,0 +1,16 @@
+import Color from '../Color.js';
+
+const Green = {
+  green:      new Color(0.0, 1.0, 0.0),
+  darkgreen:  new Color(0.0, 0.39, 0.0),
+  limegreen:  new Color(0.20, 0.80, 0.20),
+  forestgreen:new Color(0.13, 0.55, 0.13),
+  seagreen:   new Color(0.18, 0.55, 0.34),
+  mediumseagreen: new Color(0.24, 0.70, 0.44),
+  lightgreen: new Color(0.56, 0.93, 0.56),
+  palegreen:  new Color(0.60, 0.98, 0.60),
+  springgreen:new Color(0.0, 1.0, 0.50),
+  mintcream:  new Color(0.96, 1.0, 0.98),
+};
+
+export default Green;
