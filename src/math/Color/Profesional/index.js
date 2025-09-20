de5633a1a4a535d245983acc62831564214763d8
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:18:11 +0700

    Create index.js

---
+++ src/math/Color/Profesional/index.js
@@ -0,0 +1,11 @@
+import ToHex from './ToHex.js';
+import ToArray from './ToArray.js';
+import SetFromHex from './SetFromHex.js';
+import SetFromArray from './SetFromArray.js';
+
+export {
+  ToHex,
+  ToArray,
+  SetFromHex,
+  SetFromArray,
+};
