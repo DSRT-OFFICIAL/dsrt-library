bf13cf98c3a1f79edc4f01879b212c7889c62649
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:25:15 +0700

    Create Index.js

---
+++ src/math/Color/Extended/Index.js
@@ -0,0 +1,13 @@
+import Mix from './Mix.js';
+import Pastel from './Pastel.js';
+import Neon from './Neon.js';
+import Saturation from './Saturation.js';
+import Brightness from './Brightness.js';
+
+export {
+  Mix,
+  Pastel,
+  Neon,
+  Saturation,
+  Brightness,
+};
