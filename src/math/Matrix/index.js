c400b0deeed69d9efb69e79a8070aaff5dbf4fe5
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 15:39:39 +0700

    Update index.js

--- src/math/Matrix/index.js
+++ src/math/Matrix/index.js
@@ -1,6 +1,3 @@
-import Matrix2 from './Matrix2.js';
-import Matrix3 from './Matrix3.js';
-import Matrix4 from './Matrix4.js';
-
-export { Matrix2, Matrix3, Matrix4 };
-export default { Matrix2, Matrix3, Matrix4 };
+export { default as Matrix2 } from './Matrix2.js';
+export { default as Matrix3 } from './Matrix3.js';
+export { default as Matrix4 } from './Matrix4.js';
