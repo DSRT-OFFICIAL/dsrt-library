3dbc3224d78cdd8981dbe76c1c93e891f8b4610c
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 15:36:00 +0700

    Update index.js

--- src/math/index.js
+++ src/math/index.js
@@ -1,15 +1,19 @@
-// VECTORS
-export { Vector2, Vector3, Vector4 } from './vectors/index.js';
+// VECTOR
+export { Vector2, Vector3, Vector4 } from './Vector/index.js';
 
-// MATRICES
-export { Matrix2, Matrix3, Matrix4 } from './matrices/index.js';
+// MATRIX
+export { Matrix2, Matrix3, Matrix4 } from './Matrix/index.js';
 
 // QUATERNION
 export { default as Quaternion } from './Quaternion.js';
+
+// COLOR (jika ada)
+export { default as Color } from './Color/index.js';
 
 // DEFAULT EXPORT
 export default {
   Vector2, Vector3, Vector4,
   Matrix2, Matrix3, Matrix4,
-  Quaternion
+  Quaternion,
+  Color
 };
