9c74e61b4c29ab16d796bc1319b079b8b8d5a535
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:20:08 +0700

    Create ToHex.js

---
+++ src/math/Color/Profesional/ToHex.js
@@ -0,0 +1,10 @@
+import Color from '../Color.js';
+
+function toHex(c) {
+  const r = Math.round(c.r * 255).toString(16).padStart(2, '0');
+  const g = Math.round(c.g * 255).toString(16).padStart(2, '0');
+  const b = Math.round(c.b * 255).toString(16).padStart(2, '0');
+  return `#${r}${g}${b}`;
+}
+
+export default toHex;
