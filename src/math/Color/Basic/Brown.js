f5311a7c7683e35ccd59b1a2ae349ecac80be2ab
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:11:05 +0700

    Create Brown.js

---
+++ src/math/Color/Basic/Brown.js
@@ -0,0 +1,16 @@
+import Color from '../Color.js';
+
+const Brown = {
+  brown:       new Color(0.65, 0.16, 0.16),
+  saddlebrown: new Color(0.55, 0.27, 0.07),
+  sienna:      new Color(0.63, 0.32, 0.18),
+  chocolate:   new Color(0.82, 0.41, 0.12),
+  peru:        new Color(0.80, 0.52, 0.25),
+  rosybrown:   new Color(0.74, 0.56, 0.56),
+  tan:         new Color(0.82, 0.71, 0.55),
+  burlywood:   new Color(0.87, 0.72, 0.53),
+  wheat:       new Color(0.96, 0.87, 0.70),
+  moccasin:    new Color(1.0, 0.89, 0.71),
+};
+
+export default Brown;
