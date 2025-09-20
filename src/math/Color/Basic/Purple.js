2b618cbd1731ba07be2532326d393b6d6a8232a9
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:11:49 +0700

    Create Purple.js

---
+++ src/math/Color/Basic/Purple.js
@@ -0,0 +1,16 @@
+import Color from '../Color.js';
+
+const Purple = {
+  purple:       new Color(0.50, 0.0, 0.50),
+  indigo:       new Color(0.29, 0.0, 0.51),
+  darkmagenta:  new Color(0.55, 0.0, 0.55),
+  mediumorchid: new Color(0.73, 0.33, 0.83),
+  violet:       new Color(0.93, 0.51, 0.93),
+  plum:         new Color(0.87, 0.63, 0.87),
+  lavender:     new Color(0.90, 0.90, 0.98),
+  thistle:      new Color(0.85, 0.75, 0.85),
+  orchid:       new Color(0.85, 0.44, 0.84),
+  mediumslateblue: new Color(0.48, 0.41, 0.93),
+};
+
+export default Purple;
