d2a45b509c656ef38c4144049afb94256ac496f3
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:24:51 +0700

    Create Brightness.js

---
+++ src/math/Color/Extended/Brightness.js
@@ -0,0 +1,10 @@
+import Color from '../Color.js';
+
+function brightness(c, factor = 1) {
+  const r = Math.min(c.r * factor, 1);
+  const g = Math.min(c.g * factor, 1);
+  const b = Math.min(c.b * factor, 1);
+  return new Color(r, g, b, c.a);
+}
+
+export default brightness;
