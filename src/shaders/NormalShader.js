aecabd089a23076513f9f02490febd278f3c042b
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 07:24:16 +0700

    Create NormalShader.js

---
+++ src/shaders/NormalShader.js
@@ -0,0 +1,26 @@
+export default {
+  vertex: /* glsl */`
+    attribute vec3 position;
+    attribute vec3 normal;
+
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+    uniform mat3 normalMatrix;
+
+    varying vec3 vNormal;
+
+    void main() {
+      vNormal = normalize(normalMatrix * normal);
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragment: /* glsl */`
+    precision mediump float;
+
+    varying vec3 vNormal;
+
+    void main() {
+      gl_FragColor = vec4(normalize(vNormal) * 0.5 + 0.5, 1.0);
+    }
+  `
+};
