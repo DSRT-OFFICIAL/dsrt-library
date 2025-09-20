ecca2777e20ce1a4eb8ad1c579e39be374f3b4d6
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 15:25:11 +0700

    Update Vector4.js

--- src/math/Vector/Vector4.js
+++ src/math/Vector/Vector4.js
@@ -1,14 +1,9 @@
 export default class Vector4 {
-  constructor(x=0, y=0, z=0, w=1) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.w = w;
-  }
+  constructor(x=0,y=0,z=0,w=1){ this.x=x; this.y=y; this.z=z; this.w=w; }
 
-  set(x,y,z,w) { this.x=x; this.y=y; this.z=z; this.w=w; return this; }
-  clone() { return new Vector4(this.x,this.y,this.z,this.w); }
-  copy(v) { this.x=v.x; this.y=v.y; this.z=v.z; this.w=v.w; return this; }
+  set(x,y,z,w){ this.x=x; this.y=y; this.z=z; this.w=w; return this; }
+  clone(){ return new Vector4(this.x,this.y,this.z,this.w); }
+  copy(v){ this.x=v.x; this.y=v.y; this.z=v.z; this.w=v.w; return this; }
 
   add(v){ this.x+=v.x; this.y+=v.y; this.z+=v.z; this.w+=v.w; return this; }
   addScalar(s){ this.x+=s; this.y+=s; this.z+=s; this.w+=s; return this; }
@@ -20,7 +15,7 @@ export default class Vector4 {
 
   length(){ return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z + this.w*this.w); }
   lengthSq(){ return this.x*this.x + this.y*this.y + this.z*this.z + this.w*this.w; }
-  normalize(){ const l=this.length(); return l>0 ? this.divideScalar(l) : this; }
+  normalize(){ const l=this.length(); return l>0?this.divideScalar(l):this; }
 
   dot(v){ return this.x*v.x + this.y*v.y + this.z*v.z + this.w*v.w; }
   distanceTo(v){ return Math.sqrt(this.distanceToSquared(v)); }
