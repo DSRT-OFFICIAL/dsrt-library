45b636c319a60db630f696db25e4b94c16221672
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:08:47 +0700

    Create White.js

---
+++ src/math/Color/Basic/White.js
@@ -0,0 +1,16 @@
+import Color from '../Color.js';
+
+const White = {
+  white:       new Color(1.0, 1.0, 1.0),
+  snow:        new Color(1.0, 0.98, 0.98),
+  honeydew:    new Color(0.94, 1.0, 0.94),
+  mintcream:   new Color(0.96, 1.0, 0.98),
+  azure:       new Color(0.94, 1.0, 1.0),
+  aliceblue:   new Color(0.94, 0.97, 1.0),
+  ghostwhite:  new Color(0.97, 0.97, 1.0),
+  seashell:    new Color(1.0, 0.96, 0.93),
+  ivory:       new Color(1.0, 1.0, 0.94),
+  floralwhite: new Color(1.0, 0.98, 0.94),
+};
+
+export default White;
