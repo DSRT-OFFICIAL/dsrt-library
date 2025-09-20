50ed9baf795b8545ff079a1fce591111aa238bf2
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 14:55:06 +0700

    Delete src/shaders/chunk directory

--- src/shaders/chunk/common.glsl.js
+++
@@ -1,9 +0,0 @@
-export default /* glsl */`
-vec3 inverseTransformDirection(in vec3 dir, in mat4 matrix) {
-  return normalize((vec4(dir, 0.0) * matrix).xyz);
-}
-
-float saturate(float x) {
-  return clamp(x, 0.0, 1.0);
-}
-`;
--- src/shaders/chunk/index.js
+++
@@ -1,9 +0,0 @@
-import common from './common.glsl.js';
-import lights from './lights.glsl.js';
-import fog from './fog.glsl.js';
-
-export default {
-  common,
-  lights,
-  fog,
-};
