9a62d5d4084865bae52a1a7b89a79e11f34ce1c8
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 07:25:45 +0700

    Create index.js

---
+++ src/shaders/index.js
@@ -0,0 +1,10 @@
+export { default as ShaderChunk } from './ShaderChunk.js';
+export { default as ShaderLib } from './ShaderLib.js';
+
+export { default as BasicShader } from './BasicShader.js';
+export { default as LambertShader } from './LambertShader.js';
+export { default as PhongShader } from './PhongShader.js';
+export { default as StandardShader } from './StandardShader.js';
+export { default as DepthShader } from './DepthShader.js';
+export { default as NormalShader } from './NormalShader.js';
+export { default as SkyboxShader } from './SkyboxShader.js';
