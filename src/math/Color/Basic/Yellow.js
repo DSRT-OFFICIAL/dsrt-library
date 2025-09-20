834c810782128293f6672e9853a8a14106bb0f99
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 18:12:31 +0700

    Create Yellow.js

---
+++ src/math/Color/Basic/Yellow.js
@@ -0,0 +1,16 @@
+import Color from '../Color.js';
+
+const Yellow = {
+  yellow:      new Color(1.0, 1.0, 0.0),
+  gold:        new Color(1.0, 0.84, 0.0),
+  lightyellow: new Color(1.0, 1.0, 0.88),
+  lemonchiffon:new Color(1.0, 0.98, 0.80),
+  khaki:       new Color(0.94, 0.90, 0.55),
+  palegoldenrod:new Color(0.93, 0.91, 0.67),
+  moccasin:    new Color(1.0, 0.89, 0.71),
+  papayawhip:  new Color(1.0, 0.94, 0.84),
+  lightgoldenrodyellow:new Color(0.98, 0.98, 0.82),
+  cornfloweryellow:new Color(1.0, 0.97, 0.36),
+};
+
+export default Yellow;
