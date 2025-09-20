8014898a130431b0745c62cc4439056b677d3e82
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-20 14:56:08 +0700

    Create index.js

---
+++ src/shaders/chunks/index.js
@@ -0,0 +1,4 @@
+export { default as common } from './common.glsl.js';
+export { default as lights } from './lights.glsl.js';
+export { default as fog } from './fog.glsl.js';
+export { default as envmap } from './envmap.glsl.js';
